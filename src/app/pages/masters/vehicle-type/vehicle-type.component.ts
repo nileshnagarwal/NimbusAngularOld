089c1f40a1721fbcf6d87eb5d1269025f31c02db
Nilesh Agarwal <eternalshenron@gmail.com>
2017-12-14 19:07:13 +0530

    Vehicle Type Form Created + Validations Completed

--- src/app/pages/masters/vehicle-type/vehicle-type.component.ts
+++ src/app/pages/masters/vehicle-type/vehicle-type.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-vehicle-type',
@@ -13,6 +12,61 @@ import { Component } from '@angular/core';
 })
 export class VehicleTypeComponent {
 
-  constructor(){};
+  vehicleTypeForm = new FormGroup(
+    {
+      vehicleType : new FormControl('',[
+        Validators.required,
+        Validators.maxLength(20),
+        Validators.minLength(5),
+      ]),
+      length: new FormControl('',[
+        Validators.required,
+        Validators.max(300),
+      ]),
+      width: new FormControl('',[
+        Validators.required,
+        Validators.max(30),
+      ]),
+      height: new FormControl('',[
+        Validators.required,
+        Validators.max(15),
+      ]),
+      weight: new FormControl('',[
+        Validators.required,
+        Validators.max(1000),
+      ]),
+    }
+  )
+
+  addVehicleType(vehicleTypeForm){};
+
+  get vehicleType()
+  {
+    return this.vehicleTypeForm.get('vehicleType');
+  }
+
+  get length()
+  {
+    return this.vehicleTypeForm.get('length');
+  }
+
+  get width()
+  {
+    return this.vehicleTypeForm.get('width');
+  }
+
+  get height()
+  {
+    return this.vehicleTypeForm.get('height');
+  }
+
+  get weight()
+  {
+    return this.vehicleTypeForm.get('weight');
+  }
+
+  displayErrors(length){
+    console.log(length.errors);
+  }
 
 }
